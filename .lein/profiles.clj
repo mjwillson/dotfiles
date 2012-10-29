fb15a9c16a574a2721c6afc738fcb73e9acb4a4d
Matthew Willson <matthew@swiftkey.net>
2012-10-29 14:03:06 +0000

    Leiningen profile

---
+++ .lein/profiles.clj
@@ -0,0 +1,31 @@
+{:user
+  {
+    :plugins [
+              [lein-midje "2.0.0-SNAPSHOT"]
+              [com.stuartsierra/lazytest "1.2.3"]
+     ]
+    :dependencies [
+      [clojure-complete "0.2.2" :exclusions [org.clojure/clojure]]
+      [clj-stacktrace "0.2.5"]
+      ]
+   :repositories {
+                  "stuart" "http://stuartsierra.com/maven2"
+                  }
+    :repl-options {
+      :init (do
+         (println "Running ~/profiles.clj :user :repl-options :init")
+         (set! *print-length* 10)
+         (set! *print-level* 5)
+         (use 'clojure.stacktrace)
+         (use 'clojure.pprint)
+      )
+      :timeout 90000
+    }
+    :debug true ;; see https://groups.google.com/forum/?fromgroups=#!topic/leiningen/4LBgKOaLBXw
+    :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                        'print-cause-trace)
+                       new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                       'pst)]
+                   (alter-var-root orig (constantly @new)))]}}
+  }
+}
