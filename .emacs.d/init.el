b74313a7dcab0b92ea0afc3e2c4c848666c8bea5
Matthew Willson <matthew@swiftkey.net>
2013-01-10 18:41:16 +0000

    Lots of .emacs.d fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,26 +7,21 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; (when (not package-archive-contents)
+;;   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp clojure-mode nrepl ac-nrepl clojure-project-mode 
-clojure-test-mode rainbow-delimiters)
+(defvar my-packages '(starter-kit starter-kit-lisp clojure-mode nrepl ac-nrepl clojure-project-mode
+                                  clojure-test-mode rainbow-delimiters)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-;;(require 'project-mode)
 (require 'clojure-project-mode)
 
-;;(add-to-list 'load-path "/home/matthew/.emacs.d/custom/ecb")
-;;(require 'ecb)
-
-; org-mode
+;; org-mode
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
@@ -34,28 +29,14 @@ clojure-test-mode rainbow-delimiters)
 (setq org-log-done t)
 (setq org-agenda-files (list "~/.emacs.d/org/todo.org"))
 
-
 (menu-bar-mode 1)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ecb-auto-activate t)
- '(ecb-layout-name "left8")
- '(ecb-layout-window-sizes (quote (("left8" (ecb-directories-buffer-name 0.17647058823529413 . 0.2833333333333333) (ecb-sources-buffer-name 0.17647058823529413 . 0.23333333333333334) (ecb-methods-buffer-name 0.17647058823529413 . 0.3) (ecb-history-buffer-name 0.17647058823529413 . 0.16666666666666666)) (""))))
- '(ecb-options-version "2.40")
- '(ecb-source-path (quote ("/home/matthew/code")))
- '(ecb-tip-of-the-day nil))
 
 (set-face-attribute 'default nil :font "Monaco-10")
 
 (setq shift-select-mode t)
 (global-auto-revert-mode t)
 
-                                        ; http://emacs.wordpress.com/2007/01/22/killing-yanking-and-copying-lines/
+;; http://emacs.wordpress.com/2007/01/22/killing-yanking-and-copying-lines/
 (setq kill-whole-line t)
 (defadvice yank (after indent-region activate)
   (if (member major-mode '(emacs-lisp-mode scheme-mode lisp-mode
@@ -133,9 +114,16 @@ clojure-test-mode rainbow-delimiters)
 (global-set-key [end]      'end-of-buffer)       ; [End]
 
 (global-set-key (kbd "C-z") 'undo) ; [Undo]. Ctrl-Z is otherwise bound
-                                   ; to hide the current frame, which
-                                   ; is pretty useless and easy to
-                                   ; trigger by accident
+                                        ; to hide the current frame, which
+                                        ; is pretty useless and easy to
+                                        ; trigger by accident
+
+;; nrepl config
+
+(require 'nrepl)
+(require 'ac-nrepl)
+;; workaround for https://github.com/kingtim/nrepl.el/issues/168
+(setq nrepl-connected-hook (reverse nrepl-connected-hook))
 
 ;; Allow right alt key to be used for alt on OS X -- e.g.
 ;; to enter special characters -- rather than act as META.
@@ -143,41 +131,48 @@ clojure-test-mode rainbow-delimiters)
 (setq mac-right-option-modifier nil)
 
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-mode-hook 'paredit-mode)
 
-                                        ; Stop the error buffer from popping up while working in the REPL buffer:
+(require 'rainbow-delimiters)
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
+
+;; Stop the error buffer from popping up while working in the REPL buffer:
 (setq nrepl-popup-stacktraces nil)
 
-                                        ; Make C-c C-z switch to the *nrepl* buffer in the current window:
+;; Make C-c C-z switch to the *nrepl* buffer in the current window:
 (add-to-list 'same-window-buffer-names "*nrepl*")
 
-(require 'ac-nrepl)
+;; auto-complete and ac-nrepl config
+
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 (add-hook 'nrepl-mode-hook 'auto-complete-mode)
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'auto-complete-mode)
+
 (eval-after-load "auto-complete"
-  '(progn (add-to-list 'ac-modes 'nrepl-mode)
-          (setq ac-auto-show-menu nil)))
+  '(progn (add-to-list 'ac-modes 'nrepl-mode 'nrepl-interaction-mode)
+          ;; http://cx4a.org/software/auto-complete/manual.html
+          (setq ac-auto-start t)
+          (setq ac-auto-show-menu t)))
 
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; this turns off emacs annoying 'completion list' buffer
+(setq completion-auto-help nil)
 
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; Not really sure what this added:
+;; (defun set-auto-complete-as-completion-at-point-function ()
+;;   (setq completion-at-point-functions '(auto-complete)))
+;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(require 'rainbow-delimiters)
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+;; (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-;; from http://bc.tech.coop/blog/docs/clojure-emacs.el
-
-(defun my-delete ()
-  "forwards delete a char or delete selected region automically"
-  (interactive)
-  (if (and transient-mark-mode mark-active)
-      (call-interactively 'kill-region)
-    (call-interactively 'delete-char)))
 
 ;;(global-set-key [delete] 'my-delete)
+
+(require 'paredit)
 
 (defun paredit-backward-maybe-delete-region ()
   (interactive)
@@ -195,15 +190,15 @@ clojure-test-mode rainbow-delimiters)
   (interactive)
   (if mark-active
       (call-interactively 'copy-region-as-kill)
-      (call-interactively 'paredit-copy-as-kill)))
+    (call-interactively 'paredit-copy-as-kill)))
 
 ;; include various punctuation used in clojure symbols & keywords, when skipping words:
 ;; http://stackoverflow.com/questions/7544536/arbitrary-characters-for-forward-word-backward-word-in-emacs
-(modify-syntax-entry ?- "w")
-(modify-syntax-entry ?: "w")
-(modify-syntax-entry ?* "w")
-(modify-syntax-entry ?> "w")
-(modify-syntax-entry ?< "w")
+(modify-syntax-entry ?- "w" clojure-mode-syntax-table)
+(modify-syntax-entry ?: "w" clojure-mode-syntax-table)
+(modify-syntax-entry ?* "w" clojure-mode-syntax-table)
+(modify-syntax-entry ?> "w" clojure-mode-syntax-table)
+(modify-syntax-entry ?< "w" clojure-mode-syntax-table)
 
 ;; todo: wrap square/curly variants too
 (defun paredit-wrap-sexp-maybe-from-middle ()
@@ -216,21 +211,19 @@ clojure-test-mode rainbow-delimiters)
   (call-interactively 'paredit-wrap-sexp)
   (forward-word 1))
 
-(eval-after-load 'paredit
-  '(progn
-     (define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-sexp-maybe-from-middle)
-     ;; useful for clojure
-     (define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
-     (define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)
-     ;; make M-x copy sexps as C-k kills sexps in paredit mode, except
-     ;; when region active in which case copy region:
-     (define-key paredit-mode-map (kbd "M-w") 'paredit-copy-as-kill-region-or-sexp)
-     ;; delete region via del/backspc to work in paredit
-     (define-key paredit-mode-map (kbd "<delete>") 'paredit-forward-maybe-delete-region)
-     (define-key paredit-mode-map (kbd "DEL") 'paredit-backward-maybe-delete-region)
-     ;; forward delete
-     (define-key paredit-mode-map (kbd "M-S-<delete>") 'paredit-forward-kill-word)
-     ))
+(define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-sexp-maybe-from-middle)
+;; useful for clojure
+(define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
+(define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)
+;; make M-x copy sexps as C-k kills sexps in paredit mode, except
+;; when region active in which case copy region:
+(define-key paredit-mode-map (kbd "M-w") 'paredit-copy-as-kill-region-or-sexp)
+;; delete region via del/backspc to work in paredit
+(define-key paredit-mode-map (kbd "<delete>") 'paredit-forward-maybe-delete-region)
+(define-key paredit-mode-map (kbd "DEL") 'paredit-backward-maybe-delete-region)
+;; forward delete
+(define-key paredit-mode-map (kbd "M-S-<delete>") 'paredit-forward-kill-word)
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -378,4 +371,29 @@ clojure-test-mode rainbow-delimiters)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; speedbar
-(speedbar 1)
+;; (speedbar 1)
+
+;; translucent bg from http://emacs-fu.blogspot.co.uk/2009/02/transparent-emacs.html
+(defun djcb-opacity-modify (&optional dec)
+  "modify the transparency of the emacs frame; if DEC is t,
+    decrease the transparency, otherwise increase it in 10%-steps"
+  (let* ((alpha-or-nil (frame-parameter nil 'alpha)) ; nil before setting
+         (oldalpha (if alpha-or-nil alpha-or-nil 100))
+         (newalpha (if dec (- oldalpha 10) (+ oldalpha 10))))
+    (when (and (>= newalpha frame-alpha-lower-limit) (<= newalpha 100))
+      (modify-frame-parameters nil (list (cons 'alpha newalpha))))))
+
+;; C-8 will increase opacity (== decrease transparency)
+;; C-9 will decrease opacity (== increase transparency
+;; C-0 will returns the state to normal
+(global-set-key (kbd "C-8") '(lambda()(interactive)(djcb-opacity-modify)))
+(global-set-key (kbd "C-9") '(lambda()(interactive)(djcb-opacity-modify t)))
+(global-set-key (kbd "C-0") '(lambda()(interactive)
+                               (modify-frame-parameters nil `((alpha . 100)))))
+
+;; Helps with LaTeX etc
+(add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
+;; Java
+
+(add-hook 'java-mode-hook (lambda () (setq c-basic-offset 2)))
