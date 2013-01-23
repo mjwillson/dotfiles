67dd1a039302a37d6dd23b93e0165ffa5e6f7ccf
Matthew Willson <matthew@swiftkey.net>
2013-01-23 11:37:07 +0000

    Sadly clobber some nice :repl-options :init settings which seem to trigger bugs after upgrading bits of toolchain

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,9 +6,18 @@
   :repl-options
   {:init (do
            (println "Running ~/profiles.clj :user :repl-options :init")
-           (set! *print-length* 10)
-           (set! *print-level* 5)
+
+           ;; Having to comment these out for now as they trigger
+           ;; https://github.com/trptcolin/reply/issues/99 :-(
+           ;; (set! *print-length* 10)
+           ;; (set! *print-level* 5)
+
            (use 'clojure.stacktrace)
+           ;; Substituting this for the above, as I would like to do,
+           ;; for some reason causes ac-nrepl to hang caching JVM
+           ;; class names: https://github.com/purcell/ac-nrepl/issues/25
+           ;; (require '[clj-stacktrace.repl :refer [pst]])
+
            (use 'clojure.pprint)
            (use 'clojure.reflect))
    :timeout 90000}
