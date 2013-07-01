3f32e068a4ae6cad70986fa71fb51d310ae8234c
Matthew Willson <matthew@swiftkey.net>
2013-07-01 16:42:53 +0100

    Simplify .lein/profiles.clj a bit, bump versions and reinstate *print-level* stuff now bug fixed

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,30 +1,18 @@
 {:user
  {
-  :plugins [[lein-midje "2.0.4"]]
-  :dependencies [[clojure-complete "0.2.2" :exclusions [org.clojure/clojure]]
-                 [clj-stacktrace "0.2.5"]]
+  :plugins [[lein-midje "3.0.0"]
+            [lein-exec "0.3.0"]]
+  :dependencies [[clojure-complete "0.2.3" :exclusions [org.clojure/clojure]]]
   :repl-options
   {:init (do
            (println "Running ~/profiles.clj :user :repl-options :init")
 
            ;; Having to comment these out for now as they trigger
            ;; https://github.com/trptcolin/reply/issues/99 :-(
-           ;; (set! *print-length* 10)
-           ;; (set! *print-level* 5)
-
-           (use 'clojure.stacktrace)
-           ;; Substituting this for the above, as I would like to do,
-           ;; for some reason causes ac-nrepl to hang caching JVM
-           ;; class names: https://github.com/purcell/ac-nrepl/issues/25
-           ;; (require '[clj-stacktrace.repl :refer [pst]])
+           (set! *print-length* 10)
+           (set! *print-level* 5)
 
            (use 'clojure.pprint)
-           (use 'clojure.reflect))
-   :timeout 90000}
-  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                      'print-cause-trace)
-                     new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                     'pst)]
-                 (alter-var-root orig (constantly @new)))]
-  }
- }
+           (use 'clojure.reflect)
+           (use 'clojure.repl))
+   :timeout 90000}}}
