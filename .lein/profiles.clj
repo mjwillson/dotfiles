1137602364dd8287ac36d87644295349e779057a
Matthew Willson <matthew@swiftkey.net>
2013-07-10 12:40:36 +0100

    Alias repl bindings in '. for convenience, bump midje and increase print-length

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,18 +1,23 @@
 {:user
- {
-  :plugins [[lein-midje "3.0.0"]
+ {:plugins [[lein-midje "3.0.1"]
             [lein-exec "0.3.0"]]
   :dependencies [[clojure-complete "0.2.3" :exclusions [org.clojure/clojure]]]
   :repl-options
   {:init (do
            (println "Running ~/profiles.clj :user :repl-options :init")
 
-           ;; Having to comment these out for now as they trigger
-           ;; https://github.com/trptcolin/reply/issues/99 :-(
-           (set! *print-length* 10)
+           (set! *print-length* 20)
            (set! *print-level* 5)
 
-           (use 'clojure.pprint)
-           (use 'clojure.reflect)
-           (use 'clojure.repl))
+           ;; make repl bindings available directly in 'user
+           ;; namespace, but also alias them in a '. namespace so you
+           ;; can do e.g. (./source foo) from any other namespace.
+           (doseq [[ns _ names] clojure.main/repl-requires]
+             (use ns)
+             (binding [*ns* (create-ns '.)]
+               (doseq [n names]
+                 (let [s (symbol (name ns) (name n))]
+                   (eval `(alter-meta!
+                           (def ~n @(var ~s))
+                           merge (meta (var ~s)))))))))
    :timeout 90000}}}
